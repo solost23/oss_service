4d84a5b0f7ad1eb8e627c27f77f272876308287a
tianyuan <tianyuanyuans@163.com>
2022-11-24 18:59:04 +0800

    feat: minio conf

--- configs/config.go
+++ configs/config.go
@@ -6,6 +6,7 @@ type ServerConfig struct {
 	TimeLocation string      `mapstructure:"time_location"`
 	MySQLConfig  *MySQLConf  `mapstructure:"mysql"`
 	ConsulConfig *ConsulConf `mapstructure:"consul"`
+	MinioConfig  *MinioConf  `mapstructure:"minio"`
 }
 
 type MySQLConf struct {
@@ -18,4 +19,11 @@ type MySQLConf struct {
 type ConsulConf struct {
 	Host string `mapstructure:"host" json:"host"`
 	Port int    `mapstructure:"port" json:"port"`
+}
+
+type MinioConf struct {
+	EndPoint        string `mapstructure:"end_point"`
+	AccessKeyId     string `mapstructure:"access_key_id"`
+	SecretAccessKey string `mapstructure:"secret_access_key"`
+	UserSsl         bool   `mapstructure:"user_ssl"`
 }
